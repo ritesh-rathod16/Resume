c086941915b4416bf4027d4ff50da8b2440ad000
Ritesh Rathod <riteshrathod016@gmail.com>
2025-02-22 22:34:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-document.querySelectorAll('.view-certificate').forEach(item => {
-    item.addEventListener('mouseover', event => {
-        const imgSrc = item.getAttribute('data-img');
+document.querySelectorAll('.view-certificate').forEach(link => {
+    link.addEventListener('mouseover', function() {
+        const imgSrc = this.getAttribute('data-img');
         const modal = document.getElementById('certificate-modal');
         const modalImg = document.getElementById('modal-img');
         modal.style.display = "block";
         modalImg.src = imgSrc;
     });
 
-    item.addEventListener('mouseout', event => {
+    link.addEventListener('mouseout', function() {
         const modal = document.getElementById('certificate-modal');
         modal.style.display = "none";
     });
 });
 
-document.querySelector('.close').onclick = function() {
+document.querySelector('.close').addEventListener('click', function() {
     const modal = document.getElementById('certificate-modal');
     modal.style.display = "none";
-}
+});
