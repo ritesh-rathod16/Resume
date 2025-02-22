927eb05d428ddc0837a8a50bed18caed577f55f4
Ritesh Rathod <riteshrathod016@gmail.com>
2025-02-22 22:44:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
 document.querySelectorAll('.view-certificate').forEach(link => {
     link.addEventListener('mouseover', function() {
         const imgSrc = this.getAttribute('data-img');
-        const modal = document.getElementById('certificate-modal');
-        const modalImg = document.getElementById('modal-img');
-        modal.style.display = "block";
-        modalImg.src = imgSrc;
+        const img = document.createElement('img');
+        img.src = imgSrc;
+        img.style.position = 'absolute';
+        img.style.top = '50%';
+        img.style.left = '50%';
+        img.style.transform = 'translate(-50%, -50%)';
+        img.style.zIndex = '1000';
+        img.style.width = '300px'; // Adjust size as needed
+        document.body.appendChild(img);
+        this.addEventListener('mouseout', function() {
+            document.body.removeChild(img);
+        });
     });
-
-    link.addEventListener('mouseout', function() {
-        const modal = document.getElementById('certificate-modal');
-        modal.style.display = "none";
-    });
-});
-
-document.querySelector('.close').addEventListener('click', function() {
-    const modal = document.getElementById('certificate-modal');
-    modal.style.display = "none";
 });
